ba092f6c66ad8d734d309fc4d283d0a66cb8abc9
just kadek <kdkteguhmahesa@gmail.com>
2025-09-01 10:19:09 +0800

    Update main.go, add configuration files and update .gitignore

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/dekguh/learn-go-api/configs"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	configYaml, err := configs.LoadConfig("")
+	if err != nil {
+		log.Fatalln("failed to load config:", err)
+	}
+
+	log.Println("config:", configYaml)
+
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
